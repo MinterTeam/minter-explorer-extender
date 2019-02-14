f381ac4a41313007c2cf05edb5dce2d0c31ea837
Daniil Lashin <admin@lcor.ru>
2019-02-14 12:19:13 +0300

    Refactor coin service

--- coin/service.go
+++ coin/service.go
@@ -1,6 +1,7 @@
 package coin
 
 import (
+	"encoding/json"
 	"errors"
 	"github.com/MinterTeam/minter-explorer-extender/address"
 	"github.com/MinterTeam/minter-explorer-extender/models"
@@ -20,30 +21,45 @@ func NewService(repository *Repository, addressRepository *address.Repository) *
 	}
 }
 
+type CreateCoinData struct {
+	Name           string `json:"name"`
+	Symbol         string `json:"symbol"`
+	InitialAmount  string `json:"initial_amount"`
+	InitialReserve string `json:"initial_reserve"`
+	Crr            string `json:"crr"`
+}
+
 func (s Service) CreateFromTx(tx responses.Transaction) error {
 	if tx.Data == nil {
 		return errors.New("no data for creating a coin")
 	}
-	data := *tx.Data
-	name := data["name"].(string)
-	symbol := data["symbol"].(string)
-	initialAmount := data["initial_amount"].(string)
-	initialReserve := data["initial_reserve"].(string)
+
+	// TODO: refactor
+	var data CreateCoinData
+	jsonData, err := json.Marshal(*tx.Data)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(jsonData, &data)
+	if err != nil {
+		return err
+	}
+
 	txFrom := []rune(tx.From)
 	fromId, err := s.addressRepository.FindIdOrCreate(string(txFrom[2:]))
 	if err != nil {
 		return err
 	}
-	crr, err := strconv.ParseUint(data["constant_reserve_ratio"].(string), 10, 64)
+	crr, err := strconv.ParseUint(data.Crr, 10, 64)
 	if err != nil {
 		return err
 	}
 	return s.repository.Create(&models.Coin{
 		CreationAddressID: fromId,
 		Crr:               crr,
-		Volume:            initialAmount,
-		ReserveBalance:    initialReserve,
-		Name:              name,
-		Symbol:            symbol,
+		Volume:            data.InitialAmount,
+		ReserveBalance:    data.InitialReserve,
+		Name:              data.Name,
+		Symbol:            data.Symbol,
 	})
 }
