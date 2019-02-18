cb564051a3cc12fb37d38ef28777219dc19ec212
Камиль Мухаметзянов <grkamil@yandex.ru>
2019-02-18 16:31:45 +0300

    add TxOutput field to Transaction model as single TransactionOutput

--- models/transaction.go
+++ models/transaction.go
@@ -40,7 +40,8 @@ type Transaction struct {
 	FromAddress   *Address             `json:"from_address" pg:"fk:from_address_id"`              //Relation has one to Address
 	GasCoin       *Coin                `json:"gas_coin"     pg:"fk:gas_coin_id"`                  //Relation has one to Coin
 	Validators    []*Validator         `json:"validators"   pg:"many2many:transaction_validator"` //Relation has many to Validators
-	TxOutput      []*TransactionOutput `json:"tx_output"`                                         //Relation has one to TransactionOutputs
+	TxOutputs     []*TransactionOutput `json:"tx_outputs"`
+	TxOutput      *TransactionOutput   `json:"tx_output"`
 }
 
 type SendTxData struct {
