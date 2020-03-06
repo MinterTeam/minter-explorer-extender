cd9b3b175b1504927683e7ec50440971ae4e4395
Daniil Dulin <daniildulin@gmail.com>
2020-03-06 14:13:36 +0300

    update version

--- core/main.go
+++ core/main.go
@@ -69,7 +69,7 @@ func NewExtender(env *env.ExtenderEnvironment) *Extender {
 	}
 
 	contextLogger := logger.WithFields(logrus.Fields{
-		"version": "2.2.2",
+		"version": "2.2.3",
 		"app":     "Minter Explorer Extender",
 	})
 
