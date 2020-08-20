9450f8aa47909215e203ba5245fb66cfc241a243
Daniil Dulin <daniildulin@gmail.com>
2020-08-20 15:10:27 +0300

    update models

--- models/stake.go
+++ models/stake.go
@@ -1,10 +1,13 @@
 package models
 
 type Stake struct {
-	ID             uint   `json:"id"               pg:",pk"`
-	OwnerAddressID uint   `json:"owner_address_id"`
-	ValidatorID    uint   `json:"validator_id"`
-	CoinID         uint   `json:"coin_id"          pg:",use_zero"`
-	Value          string `json:"value"            pg:"type:numeric(70)"`
-	BipValue       string `json:"bip_value"        pg:"type:numeric(70)"`
+	ID             uint       `json:"id"               pg:",pk"`
+	OwnerAddressID uint       `json:"owner_address_id"`
+	ValidatorID    uint       `json:"validator_id"`
+	CoinID         uint       `json:"coin_id"          pg:",use_zero"`
+	Value          string     `json:"value"            pg:"type:numeric(70)"`
+	BipValue       string     `json:"bip_value"        pg:"type:numeric(70)"`
+	Coin           *Coin      `json:"coins"`                                  //Relation has one to Coins
+	OwnerAddress   *Address   `json:"owner_address" pg:"fk:owner_address_id"` //Relation has one to Addresses
+	Validator      *Validator `json:"validator"`                              //Relation has one to Validators
 }
