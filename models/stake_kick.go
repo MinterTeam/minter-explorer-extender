61ca9fab210e59f8b0078251840050c510539b31
Kamil Mukhametzyanov <grkamil@yandex.ru>
2020-08-24 15:22:40 +0300

    Update waitlist model

--- models/stake_kick.go
+++ models/stake_kick.go
@@ -1,9 +1,11 @@
 package models
 
 type StakeKick struct {
-	AddressId   uint     `json:"address_id"`
-	CoinId      uint     `json:"coin_id"`
-	ValidatorId uint     `json:"validator_id"`
-	Amount      string   `json:"amount"`
-	tableName   struct{} `pg:"wait_list"`
+	tableName   struct{}   `pg:"wait_list"`
+	AddressId   uint       `json:"address_id"`
+	CoinId      uint       `json:"coin_id"`
+	ValidatorId uint       `json:"validator_id"`
+	Amount      string     `json:"amount"`
+	Coin        *Coin      `json:"coin"      pg:"fk:coin_id"`
+	Validator   *Validator `json:"validator" pg:"fk:validator_id"`
 }
