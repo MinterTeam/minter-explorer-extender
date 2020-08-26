475891a8a32a5cb35a6dee8a8ea7d131968cb6a0
Kamil Mukhametzyanov <grkamil@yandex.ru>
2020-08-26 10:10:07 +0300

    Update stake_kick.go

--- models/stake_kick.go
+++ models/stake_kick.go
@@ -5,7 +5,7 @@ type StakeKick struct {
 	AddressId   uint       `json:"address_id"`
 	CoinId      uint       `json:"coin_id"`
 	ValidatorId uint       `json:"validator_id"`
-	Amount      string     `json:"amount"`
+	Value       string     `json:"value"`
 	Coin        *Coin      `json:"coin"      pg:"fk:coin_id"`
 	Validator   *Validator `json:"validator" pg:"fk:validator_id"`
 }
