960d17df25598ec865e3a74b9d9db2aa8484722e
Daniil Dulin <daniildulin@gmail.com>
2020-10-23 13:42:53 +0300

    update models

--- models/coin.go
+++ models/coin.go
@@ -19,7 +19,7 @@ type Coin struct {
 	CreatedAt        time.Time  `json:"created_at"`
 	UpdatedAt        *time.Time `json:"updated_at"`
 	DeletedAt        *time.Time `pg:",soft_delete"`
-	OwnerAddress     Address    `pg:"fk:id; rel:has-one"`
+	OwnerAddress     Address    `pg:"fk:id"`
 }
 
 // Return coin with version
