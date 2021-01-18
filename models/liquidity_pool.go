37b66740c89ad626ff7229e83cf04b3c258ce487
Daniil Dulin <daniildulin@gmail.com>
2021-01-18 12:43:22 +0300

    update LiquidityPool model

--- models/liquidity_pool.go
+++ models/liquidity_pool.go
@@ -7,10 +7,14 @@ type LiquidityPool struct {
 	FirstCoinVolume  string `json:"first_coin_volume"`
 	SecondCoinVolume string `json:"second_coin_volume"`
 	Liquidity        string `json:"liquidity"`
+	FirstCoin        *Coin  `json:"first_coin"  pg:"fk:first_coin_id"`
+	SecondCoin       *Coin  `json:"second_coin" pg:"fk:second_coin_id"`
 }
 
 type AddressLiquidityPool struct {
-	LiquidityPoolId uint64 `json:"liquidity_pool_id"`
-	AddressId       uint64 `json:"address_id"`
-	Liquidity       string `json:"liquidity"`
+	LiquidityPoolId uint64         `json:"liquidity_pool_id"`
+	AddressId       uint64         `json:"address_id"`
+	Liquidity       string         `json:"liquidity"`
+	Address         *Address       `json:"first_coin"      pg:"fk:address_id"`
+	LiquidityPool   *LiquidityPool `json:"liquidity_pool"  pg:"fk:liquidity_pool_id"`
 }
