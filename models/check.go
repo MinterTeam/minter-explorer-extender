076985ad11841d33306bd569f882b8c5a2b0caec
Daniil Dulin <daniildulin@gmail.com>
2021-01-21 12:00:48 +0300

    update check model

--- models/check.go
+++ models/check.go
@@ -1,10 +1,11 @@
 package models
 
 type Check struct {
-	TransactionID uint64  `json:"transaction_id"`
-	Data          string  `json:"data"`
-	FromAddressId uint    `json:"from_address_id"`
-	ToAddressId   uint    `json:"to_address_id"`
-	FromAddress   Address `json:"from_address" pg:"fk:from_address_id"`
-	ToAddress     Address `json:"to_address"   pg:"fk:to_address_id"`
+	TransactionID uint64       `json:"transaction_id"`
+	Data          string       `json:"data"`
+	FromAddressId uint         `json:"from_address_id"`
+	ToAddressId   uint         `json:"to_address_id"`
+	FromAddress   Address      `json:"from_address" pg:"fk:from_address_id"`
+	ToAddress     Address      `json:"to_address"   pg:"fk:to_address_id"`
+	Transaction   *Transaction `json:"transaction"  pg:"fk:transaction_id"`
 }
