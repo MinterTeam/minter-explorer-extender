e426ed4f77461cc4456c2d3b5020015dcc16384f
Daniil Dulin <daniildulin@gmail.com>
2021-03-16 15:40:04 +0300

    fix

--- models/balance.go
+++ models/balance.go
@@ -1,9 +1,9 @@
 package models
 
 type Balance struct {
-	AddressID uint     `json:"address_id"`
+	AddressID uint     `json:"address_id"  pg:",pk"`
 	CoinID    uint     `json:"coin_id"     pg:",pk,use_zero"`
-	Value     string   `json:"value"       pg:",pk,type:numeric(70)"`
+	Value     string   `json:"value"       pg:"type:numeric(70)"`
 	Address   *Address `pg:"rel:has-one"`            //Relation has one to Address
 	Coin      *Coin    `pg:"rel:has-one,fk:coin_id"` //Relation has one to Coin
 }
