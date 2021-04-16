43d0df2508de5d215f045a81f5090d57124bb2da
Daniil Dulin <daniildulin@gmail.com>
2021-04-16 11:49:55 +0300

    add pk to validator ban model

--- models/validator_bans.go
+++ models/validator_bans.go
@@ -1,8 +1,8 @@
 package models
 
 type ValidatorBan struct {
-	ValidatorId uint       `json:"validator_id"`
-	BlockId     uint64     `json:"block_id"`
-	Validator   *Validator `json:"validator" pg:"rel:belongs-to"`
-	Block       *Block     `json:"block"     pg:"rel:belongs-to"`
+	ValidatorId uint       `json:"validator_id" pg:",pk"`
+	BlockId     uint64     `json:"block_id"     pg:",pk"`
+	Validator   *Validator `json:"validator"    pg:"rel:belongs-to"`
+	Block       *Block     `json:"block"        pg:"rel:belongs-to"`
 }
