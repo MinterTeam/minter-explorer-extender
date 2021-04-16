38397f80b0d6aba3e50e52044d4d1639a6279fb4
Daniil Dulin <daniildulin@gmail.com>
2021-04-16 10:21:23 +0300

    update db update validator ban model

--- models/validator_bans.go
+++ models/validator_bans.go
@@ -1,6 +1,6 @@
 package models
 
 type ValidatorBan struct {
-	ValidatorId uint   `json:"validator_id"`
+	ValidatorId uint   `json:"validator_id"  pg:"rel:belongs-to"`
 	BlockId     uint64 `json:"block_id"`
 }
