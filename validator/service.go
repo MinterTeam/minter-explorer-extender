51312e6682bf50bdf1a3e64f563b2de82cbb742d
Daniil Dulin <daniildulin@gmail.com>
2022-02-07 15:23:55 +0300

    update stake when chasing mode in testnet

--- validator/service.go
+++ validator/service.go
@@ -232,14 +232,17 @@ func (s *Service) UpdateValidatorsWorker(jobs <-chan int) {
 
 func (s *Service) UpdateStakesWorker(jobs <-chan int) {
 	for height := range jobs {
-		status, err := s.nodeApi.Status()
-		if err != nil {
-			s.logger.Error(err)
-			continue
+		if s.env.BaseCoin != "MNT" {
+			status, err := s.nodeApi.Status()
+			if err != nil {
+				s.logger.Error(err)
+				continue
+			}
+			if status.LatestBlockHeight-uint64(height) > 240 {
+				continue
+			}
 		}
-		if status.LatestBlockHeight-uint64(height) > 240 {
-			continue
-		}
+
 		start := time.Now()
 		resp, err := s.nodeApi.CandidatesExtended(true, false, "")
 		if err != nil {
